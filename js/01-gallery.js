567aca21c9cdf6dff6a04b9fba8fc6b30ba0b0ba
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-15 00:03:45 +0200

    elements markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery")
+
+const galleryMarkup = galleryItems.map(({preview, original, description}) =>
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`).join('')
+
+galleryEl.innerHTML = galleryMarkup;
+
+// console.log(galleryMarkup);
+
+
+// console.log(galleryItems);
