a3eb497b784530c67cba85d71af071adba9751fd
Milosska <110258611+Milosska@users.noreply.github.com>
2022-12-15 02:22:33 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Step 1 - HTML markup
+const galleryEl = document.querySelector(".gallery")
+
+const galleryMarkup = galleryItems.map(({preview, original, description}) =>
+`<li>
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join('')
+
+galleryEl.innerHTML = galleryMarkup;
+
+
+// Step 2 - "click" delegation
+galleryEl.addEventListener ("click", onPreviewClick)
+
+function onPreviewClick(event) {
+    event.preventDefault()
+  }
+
+// Step 3 - library use
+
+const gallery = new SimpleLightbox(".gallery a", {captionsData: "alt", captionDelay: 250});
+
